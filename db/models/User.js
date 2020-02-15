cbb607b7ad1bf2dd717fb0c9a18d7e3fd471e0a8
ALau2088 <alau12004@yahoo.com>
2020-02-15 15:29:07 -0800

    added User schema

---
+++ db/models/User.js
@@ -0,0 +1,49 @@
+const mongoose = require('mongoose');
+const Schema = mongoose.Schema;
+
+const User = new Schema({
+  type: {
+    type: String,
+    required: true
+  },
+  username: {
+    type: String,
+    required: true
+  },
+  password: {
+    type: String,
+    required: true
+  },
+  companyName: {
+    type: String,
+    required: true
+  },
+  description: {
+    type: String,
+    required: true
+  },
+  address: {
+    type: String,
+    required: true
+  },
+  geoLocation: {
+    type: String,
+    required: true
+  },
+  pointOfContact: {
+    name: {
+      type: String,
+      required: true
+    },
+    email: {
+      type: String,
+      required: true
+    },
+    phoneNumber: {
+      type: String,
+      required: true
+    }
+  }
+});
+
+module.exports = User = mongoose.model('User', schema);
