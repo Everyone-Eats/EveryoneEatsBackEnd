cb0f29c2295edc2096ac1adecc6ff35b6877280c
Preston Whitehall <pmwhitehall@gmail.com>
2020-02-15 15:42:02 -0800

    Adds Donation db schema

---
+++ db/models/Donation.js
@@ -0,0 +1,32 @@
+const mongoose = require('mongoose');
+const Schema = { mongoose };
+
+const Donation = new Schema({
+  donor: {
+    type: String,
+    required: true,
+  },
+  category: {
+    type: String,
+    required: true,
+  },
+  description: {
+    type: String,
+    required: true,
+  },
+  costBasis: {
+    type: Number,
+    required: true,
+  },
+  quantity: {
+    type: Number,
+    required: true,
+  },
+  isAvailable: {
+    type: Boolean,
+    required: true,
+  },
+  date: { type: Date, default: Date.now, required: true, },
+});
+
+module.exports = Donation = mongoose.model('Donation', schema);
