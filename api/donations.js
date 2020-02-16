86b8c91482abd540f5af90c2b1c10c8c0a440939
jared <jcraig1730@gmail.com>
2020-02-15 16:56:33 -0800

    Setup basic crud routes for user and donation routes

--- api/donations.js
+++ api/donations.js
@@ -1,15 +1,62 @@
 const express = require("express");
 const router = express.Router();
+import { Donation } from "../db";
+import { response } from "express";
 
-const createDonation = async (req, res) => {};
+const createDonation = async (req, res) => {
+  try {
+    const donationData = req.body;
+    const newDonation = await new Donation(donationData);
+    res.status(201).json(newDonation);
+  } catch (err) {
+    res.status(400);
+    console.log(err);
+  }
+};
 
-const updateDonation = async (req, res) => {};
+const updateDonation = async (req, res) => {
+  try {
+    const { id } = req.params;
+    const updatedData = req.body;
+    const updatedDonation = await Donation.findByIdAndUpdate(id, updatedData);
+    res.status(201).json(updatedDonation);
+  } catch (err) {
+    res.status(400);
+    console.log(err);
+  }
+};
 
-const getAllDonations = async (req, res) => {};
+const getAllDonations = async (req, res) => {
+  try {
+    const donations = await Donation.find();
+    res.status(200).json(donations);
+  } catch (err) {
+    res.status(400);
+    console.log(err);
+  }
+};
 
-const getDonation = async (req, res) => {};
+const getDonation = async (req, res) => {
+  try {
+    const { id } = req.params;
+    const donation = await Donation.findById(id);
+    res.status(200).json(donation);
+  } catch (err) {
+    res.status(400);
+    console.log(err);
+  }
+};
 
-const deleteDonation = async (req, res) => {};
+const deleteDonation = async (req, res) => {
+  try {
+    const { id } = req.params;
+    await Donation.findByIdAndDelete(id);
+    res.status(204);
+  } catch (err) {
+    res.status(400);
+    console.log(err);
+  }
+};
 
 router.post("/", createDonation);
 router.put("/:id", updateDonation);
--- api/users.js
+++ api/users.js
@@ -1,23 +1,71 @@
 const express = require("express");
 const router = express.Router();
+const { User } = require("../db");
 
-const test = (req, res) => {
-  res.send("idiot check");
+const createUser = async (req, res) => {
+  try {
+    const userData = req.body;
+    const newUser = await new User(userData).save();
+    res.status(201).json(newUser);
+  } catch (err) {
+    res.status(400);
+    console.log(err);
+  }
 };
 
-const createUser = async (req, res) => {};
+const updateUser = async (req, res) => {
+  try {
+    const updatedData = req.body;
+    const { id } = req.params;
+    const updatedUser = await User.findByIdAndUpdate(id, updatedData);
+    res.status(201).json(updatedUser);
+  } catch (err) {
+    res.status(400);
+    console.log(err);
+  }
+};
 
-const updateUser = async (req, res) => {};
+const getAllUsers = async (req, res) => {
+  try {
+    const users = await User.find();
+    console.log(2);
+    res.status(200).json(users);
+  } catch (err) {
+    console.log("in err");
+    res.setStatus(400);
+    console.log(err);
+  }
+};
 
-const getAllUsers = async (req, res) => {};
+const getUser = async (req, res) => {
+  console.log("hi");
+  try {
+    const { id } = req.params;
+    console.log("here");
+    const users = await User.findById(id);
+    console.log("here");
+    res.status(200).json(users);
+  } catch (err) {
+    res.setStatus(400);
+    console.log(err);
+  }
+};
 
-const getUser = async (req, res) => {};
+const deleteUser = async (req, res) => {
+  try {
+    const { id } = req.params;
+    await User.findByIdAndDelete(id);
+    res.status(204);
+  } catch (err) {
+    res.status(400);
+    console.log(err);
+  }
+};
 
-const deleteUser = async (req, res) => {};
-
-router.get("/test", test);
 router.post("/", createUser);
 router.put("/:id", updateUser);
+router.delete(":/id", deleteUser);
 router.get("/:id", getUser);
 router.get("/", getAllUsers);
+
 module.exports = router;
