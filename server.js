b43ddbb73e1c5c52ffb0a03c5453104aa53a8eea
Whitehall <pwhitehall@us9801a7a20bbf.am.sony.com>
2020-03-11 15:21:48 -0700

    Fixes errors introduced during a previous merge conflict

--- server.js
+++ server.js
@@ -1,10 +1,9 @@
 const express = require('express');
 const cors = require('cors');
+const app = express();
 
 app.use(express.urlencoded({ extended: true }));
 app.use(express.json());
-app.use(cors());
-
 app.use(cors());
 
 app.use('/api', require('./api'));
